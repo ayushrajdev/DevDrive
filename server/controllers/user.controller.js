ada9daf87f51c35dc4712db7f0b1750f11abfccf
ayushrajdev <ayushraj.dev10@gmail.com>
2026-02-20 00:13:52 +0530

    added transcation in mongoose

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,18 +1,21 @@
-import { Db, MongoClient, ObjectId } from 'mongodb';
+import { ObjectId } from 'mongodb';
 import { successResponse } from '../Response.js';
-// import { client } from "../config/db.config.js";
 import User from '../models/user.model.js';
 import Directory from '../models/directory.model.js';
+import mongoose, { Types } from 'mongoose';
 
 async function registerUser(req, res, next) {
   const { db } = req;
+  const session = await mongoose.startSession();
   try {
     const { email, password, name } = req.body;
     if (!email || !password) {
       res.status(409).json({ message: 'invalid crediantials' });
     }
-    const oidUser = new ObjectId();
-    const oidDirectory = new ObjectId();
+    const oidUser = new Types.ObjectId();
+    const oidDirectory = new Types.ObjectId();
+
+    session.startTransaction();
 
     const user = new User({
       _id: oidUser,
@@ -28,11 +31,14 @@ async function registerUser(req, res, next) {
     });
     await user.save();
 
+    session.commitTransaction();
+
     res.cookie('uid', oidUser.toString());
     res.status(201).json({ user, directory });
   } catch (error) {
     console.log(error);
     next(error);
+    session.abortTransaction();
   }
 }
 
