0a131e44d1ac9696ef2433861da395f429d23724
ayushrajdev <ayushraj.dev10@gmail.com>
2026-02-21 17:24:08 +0530

    implemented custom jwt

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,93 +1,115 @@
-import { ObjectId } from 'mongodb';
 import { successResponse } from '../Response.js';
 import User from '../models/user.model.js';
 import Directory from '../models/directory.model.js';
 import mongoose, { Types } from 'mongoose';
 
 async function registerUser(req, res, next) {
-  const { db } = req;
-  const session = await mongoose.startSession();
-  try {
-    const { email, password, name } = req.body;
-    if (!email || !password) {
-      res.status(409).json({ message: 'invalid crediantials' });
+    const { db } = req;
+    const session = await mongoose.startSession();
+    try {
+        const { email, password, name } = req.body;
+        if (!email || !password) {
+            res.status(409).json({ message: 'invalid crediantials' });
+        }
+        console.log(email, password, name);
+        const oidUser = new Types.ObjectId();
+        const oidDirectory = new Types.ObjectId();
+
+        session.startTransaction();
+
+        const user = await User.insertOne(
+            {
+                _id: oidUser,
+                name,
+                email,
+                password,
+                rootDirId: oidDirectory,
+            },
+            { session },
+        );
+        const directory = await Directory.insertOne(
+            {
+                _id: oidDirectory,
+                name: `root-${email}`,
+                userId: oidUser,
+            },
+            { session },
+        );
+
+        session.commitTransaction();
+
+        // const token =
+        //     oidUser.toString() +
+        //     Math.round(new Date.now() / 1000 + 4000).toString('16');
+
+        // res.cookie('uid', token);
+        res.status(201).json({ user, directory });
+    } catch (error) {
+        if (error.code == 11000) {
+            res.status(409).json({ error: error.message });
+        }
+        console.log(error);
+        next(error);
+        session.abortTransaction();
     }
-    const oidUser = new Types.ObjectId();
-    const oidDirectory = new Types.ObjectId();
-
-    session.startTransaction();
-
-    const user = new User({
-      _id: oidUser,
-      name,
-      email,
-      password,
-      rootDirId: oidDirectory,
-    });
-    const directory = await Directory.create({
-      _id: oidDirectory,
-      name: `root-${email}`,
-      userId: oidUser,
-    });
-    await user.save();
-
-    session.commitTransaction();
-
-    res.cookie('uid', oidUser.toString());
-    res.status(201).json({ user, directory });
-  } catch (error) {
-    console.log(error);
-    next(error);
-    session.abortTransaction();
-  }
 }
 
 async function loginUser(req, res) {
-  try {
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    if (!email || !password) {
-      return res.status(400).json({ message: 'invalid crendiatels' });
+        if (!email || !password) {
+            return res.status(400).json({ message: 'invalid crendiatels' });
+        }
+
+        const user = await User.findOne({
+            email,
+        });
+
+        if (!user || user.password !== password) {
+            return res.status(400).json({ message: 'invalid email' });
+        }
+        console.log(user);
+        // const token =
+        //     user._id.toString() +
+        //     Math.round(Date.now() / 1000 + 4000).toString('16');
+        const customJWT = JSON.stringify({
+            expiry: Math.round(Date.now() / 1000 + 4000).toString('16'),
+            id: user._id.toString(),
+        });
+
+        res.cookie('uid', Buffer.from(customJWT).toString('base64url'), {
+            httpOnly: true,
+        });
+        res.json({ message: 'loggineed', user });
+    } catch (error) {
+        console.log(error.message);
     }
-
-    const user = await User.findOne({
-      email,
-    });
-
-    if (!user || user.password !== password) {
-      return res.status(400).json({ message: 'invalid email' });
-    }
-    console.log(user);
-    res.cookie('uid', user._id.toString(), {
-      httpOnly: true,
-    });
-    res.json({ message: 'loggineed', user });
-  } catch (error) {}
 }
 
 async function logoutUser(req, res, next) {
-  try {
-    res.clearCookie('uid');
-    return successResponse(res, 'logged out successfull');
-  } catch (error) {
-    next(error);
-  }
+    try {
+        res.clearCookie('uid');
+        return successResponse(res, 'logged out successfull');
+    } catch (error) {
+        next(error);
+    }
 }
 
 async function getUserInfo(req, res, next) {
-  try {
-    const { user } = req;
-    return res.status(200).json({ user });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const { user } = req;
+        return res.status(200).json({ user });
+    } catch (error) {
+        next(error);
+    }
 }
 
 const userController = {
-  registerUser,
-  loginUser,
-  logoutUser,
-  getUserInfo,
+    registerUser,
+    loginUser,
+    logoutUser,
+    getUserInfo,
 };
 
 export default userController;
--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -1,42 +1,53 @@
 import User from '../models/user.model.js';
 import { errorResponse } from '../Response.js';
-import { ObjectId } from 'mongodb';
 
-export async function checkIsLoggedIn(req, res, next) {
-  const { uid } = req.cookies;
-  if (!uid) return errorResponse(res);
+export async function checkUserAuth(req, res, next) {
+    const { uid } = req.cookies;
+    if (!uid) return errorResponse(res);
+    console.log(Buffer.from(uid))
+    const { expiry, id } = JSON.parse(Buffer.from(
+        uid,
+        'base64url',
+    ).toString('utf8'));
+    const expiryTimeInSecond = parseInt(expiry, 16);
+    const currentTimeInSecond = Math.floor(Number(Date.now() / 1000));
+    console.log({ currentTimeInSecond, expiryTimeInSecond });
+    if (expiryTimeInSecond - currentTimeInSecond) {
+        res.clearCookie('uid');
+        return res.end();
+    }
+    const user = await User.findById(id);
+    if (!user) return errorResponse(res);
 
-  const user = await User.findById(uid);
-  if (!user) return errorResponse(res);
-  req.uid = uid;
-  req.user = user;
-  console.log(uid);
-  console.log(user);
+    req.uid = uid;
+    req.user = user;
+    console.log(uid);
+    console.log(user);
 
-  next();
+    next();
 }
 
 export function dirIdOfCurrentUser(req, res, next) {
-  try {
-    // const { user, uid } = req;
-    // const parentDirId = req.headers.parentdirid || user.rootDirID;
-    // console.log("Header:", req.headers.parentdirid);
-    // console.log("Type:", typeof req.headers.parentdirid);
+    try {
+        // const { user, uid } = req;
+        // const parentDirId = req.headers.parentdirid || user.rootDirID;
+        // console.log("Header:", req.headers.parentdirid);
+        // console.log("Type:", typeof req.headers.parentdirid);
 
-    // const DirectoryOfUser = directoriesData.find(
-    //   (dir) => dir.id == parentDirId && dir.userId == uid,
-    // );
+        // const DirectoryOfUser = directoriesData.find(
+        //   (dir) => dir.id == parentDirId && dir.userId == uid,
+        // );
 
-    // console.log(parentDirId);
-    // console.log(DirectoryOfUser);
-    // if (!DirectoryOfUser) {
-    //   return errorResponse(res, "this directory is not of yours");
-    // }
+        // console.log(parentDirId);
+        // console.log(DirectoryOfUser);
+        // if (!DirectoryOfUser) {
+        //   return errorResponse(res, "this directory is not of yours");
+        // }
 
-    // req.parentDirId = parentDirId;
-    // req.DirectoryOfUser = DirectoryOfUser;
-    next();
-  } catch (error) {
-    next(error);
-  }
+        // req.parentDirId = parentDirId;
+        // req.DirectoryOfUser = DirectoryOfUser;
+        next();
+    } catch (error) {
+        next(error);
+    }
 }
